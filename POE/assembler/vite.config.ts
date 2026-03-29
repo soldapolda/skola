75e30f64079dc682d172915b43f88d658c455e00
vojtech <solanskyv@seznam.cz>
2026-03-29 23:00:09 +0200

    deploy assembler

--- POE/assembler/vite.config.ts
+++ POE/assembler/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/skola/POE/assembler-sim/',
+  base: '/skola/POE/assembler/',
   plugins: [react(), tailwindcss()],
 })
